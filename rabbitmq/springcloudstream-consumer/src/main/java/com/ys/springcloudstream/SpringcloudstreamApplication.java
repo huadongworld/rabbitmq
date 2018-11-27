866d68d3e77064827ebb1a3e9beb4dd3def7dae9
huadong <hd2260332898@gmail.com>
2018-11-27 23:14:44 +0800

    构建springcloudstream-producer模块

---
+++ rabbitmq/springcloudstream-consumer/src/main/java/com/ys/springcloudstream/SpringcloudstreamApplication.java
@@ -0,0 +1,16 @@
+package com.ys.springcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HuaDong
+ * @date 2018/11/27 23:13
+ */
+@SpringBootApplication
+public class SpringcloudstreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudstreamApplication.class, args);
+    }
+}
